a3bdf88d595aa41006cf0ee4947d930b28f09f2d
marboqmar <martamartinezboquer@gmail.com>
2024-04-01 12:06:54 +0200

    Created basic HTML for cart

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,6 +1,51 @@
+const CartItem = () => {
+   return (
+       <div className={'cartItem'}>
+           <img className={'cartItemImg'} src={'/product-image.png'} alt={'cart item image'} />
+           <div>
+               <h2>La mejor espada del mundo</h2>
+               <div>
+                   <span>Cantidad: 1</span>
+                   <button>Eliminar</button>
+                   <span>160€</span>
+               </div>
+           </div>
+       </div>
+   );
+};
+
+const Total = () => {
+    return (
+        <div className={'total'}>
+            <div>
+                <span>Artículos (2)</span>
+                <span>320€</span>
+            </div>
+            <div>
+                <span>Envío</span>
+                <span>Gratis</span>
+            </div>
+            <div className={'totalPrice'}>
+                <h2>Total</h2>
+                <h2>320€</h2>
+            </div>
+            <button>Pagar</button>
+        </div>
+    );
+};
+
 const Cart = () => {
     return (
-        <h1>Cesta</h1>
+        <div>
+            <h1>Cesta</h1>
+            <div className={'cartDisplay'}>
+                <div>
+                    <CartItem />
+                    <CartItem />
+                </div>
+                <Total />
+            </div>
+        </div>
     );
 };
 
