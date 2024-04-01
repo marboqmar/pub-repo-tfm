16ac1f111e1f8ffcaeb972b223e9f4284886b2de
marboqmar <martamartinezboquer@gmail.com>
2024-04-01 13:18:11 +0200

    Created favourites basic HTML

--- src/Favourites.tsx
+++ src/Favourites.tsx
@@ -1,6 +1,15 @@
+import ItemDisplay from "./components/ItemDisplay.tsx";
+
 const Favourites = () => {
     return (
-        <h1>Favoritos</h1>
+        <div>
+            <div className={'favouritesTopBar'}>
+                <h1>Favoritos</h1>
+                <input className={'favouritesSearchBar'} id={'favouritesSearchBar'} type={'text'} placeholder={'Buscar en favoritos'} />
+                <button className={'favouritesSearchButton'}>Buscar en favoritos</button>
+            </div>
+            <ItemDisplay />
+        </div>
     );
 };
 
