5b0829a61a73af73418e5af702141ffc68fcba56
marboqmar <martamartinezboquer@gmail.com>
2024-04-19 10:12:02 +0200

    Translations working, set up translations for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
         <div>
             <Link to={'/'}><img className={'logo'} src={'/logo-header.png'} alt={'Website logo'}/></Link>
             <Link className={'categoriesMenu'} to={'/categoria'}>{t('common:header.categories')}</Link>
-            <input id={'searchBar'} type={'text'} placeholder={'Buscar'} />
-            <Link className={'myAccountMenu'} to={'mi-cuenta'}>Mi cuenta</Link>
-            <Link to={'/favoritos'}>Favoritos</Link>
-            <Link to={'/cesta'}>Cesta</Link>
+            <input id={'searchBar'} type={'text'} placeholder={t('common:header.searchBarPlaceholder')} />
+            <Link className={'myAccountMenu'} to={'mi-cuenta'}>{t('common:header.myAccount')}</Link>
+            <Link to={'/favoritos'}>{t('common:header.favourites')}</Link>
+            <Link to={'/cesta'}>{t('common:header.cart')}</Link>
         </div>
     );
 };
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,17 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
+    .use(Backend)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
-        lng: "es", // default language
-
+        lng: "en", // default language
+        fallbackLng: 'en',
         interpolation: {
             escapeValue: false // react already safes from xss
         }
