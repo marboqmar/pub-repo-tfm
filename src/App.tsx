ef406ffca994d55b2a85f8c920fda33fbde95d25
marboqmar <martamartinezboquer@gmail.com>
2024-05-02 13:29:22 +0200

    Filters generated from a list and translate properly

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './scss/style.scss'
 // import { Link } from 'react-router-dom'
 import Header from './components/Header/Header.tsx'
 import Footer from "./components/Footer/Footer.tsx";
-import {Outlet, useLocation} from "react-router-dom";
-import {useEffect} from "react";
+import {Outlet} from "react-router-dom";
+// import {Outlet, useLocation} from "react-router-dom";
+// import {useEffect} from "react";
 
 function App() {
     // const {pathname} = useLocation();
