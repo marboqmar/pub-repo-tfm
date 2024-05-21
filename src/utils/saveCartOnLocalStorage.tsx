f5ac0ed2bf8a827b570ce8ebc208b587cdd911f8
marboqmar <martamartinezboquer@gmail.com>
2024-05-21 18:11:22 +0200

    Uncommented useLocalStorage

--- src/utils/saveCartOnLocalStorage.tsx
+++ src/utils/saveCartOnLocalStorage.tsx
@@ -1,29 +1,29 @@
 import { JSONCartModel } from "../models/cartModel.ts";
 const LOCAL_STORAGE_CART_KEY = "Cart";
-// import useLocalStorage from "use-local-storage";
+import useLocalStorage from "use-local-storage";
 
 export const saveCartOnLocalStorage = (cart: JSONCartModel[]) => {
-  localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cart));
+  // localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cart));
 
-  // const options = {
-  //   serializer: (object) => {
-  //     return JSON.stringify(object);
-  //   },
-  // };
-  //
-  // const [cartItems, setCartItems] = useLocalStorage<string>(
-  //   LOCAL_STORAGE_CART_KEY,
-  //   "",
-  //   options,
-  // );
-  //
-  // console.log(options.serializer(cart));
-  //
-  // setCartItems(options.serializer(cart));
-  //
-  // return {
-  //   cartItems,
-  //   setCartItems,
-  //   options,
-  // };
+  const options = {
+    serializer: (object) => {
+      return JSON.stringify(object);
+    },
+  };
+
+  const [cartItems, setCartItems] = useLocalStorage<string>(
+    LOCAL_STORAGE_CART_KEY,
+    "",
+    options,
+  );
+
+  console.log(options.serializer(cart));
+
+  setCartItems(options.serializer(cart));
+
+  return {
+    cartItems,
+    setCartItems,
+    options,
+  };
 };
