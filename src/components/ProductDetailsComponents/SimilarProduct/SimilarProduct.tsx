6ac5772dce330590ed3f7c5d763f17ea40a2d4c6
marboqmar <martamartinezboquer@gmail.com>
2024-06-05 10:15:38 +0200

    Removed calls to useCallApi (former useShopItemsList) to avoid extra calls to API. Returned useCallApi returns in useApiResultsAndFilteredItems and use those return throughout the project from the latter.

--- src/components/ProductDetailsComponents/SimilarProduct/SimilarProduct.tsx
+++ src/components/ProductDetailsComponents/SimilarProduct/SimilarProduct.tsx
@@ -1,11 +1,12 @@
-import { Button } from "../Button/Button.tsx";
+import "./SimilarProduct.scss";
+import { Button } from "../../Button/Button.tsx";
 import { Link } from "react-router-dom";
 
 export const SimilarProduct = () => {
   return (
     <Button
       component={Link}
-      className={"similarProduct--item flex-column"}
+      className={"similarProduct--item"}
       color={"none"}
       paddingSize={"none"}
       withoutHover
